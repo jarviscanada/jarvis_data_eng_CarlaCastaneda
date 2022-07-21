f3c62835fa88eec1a74034a237cb3537a8e84b2b
Carla <cast4730@mylaurier.ca>
2022-07-21 20:55:38 +0000

    5 min interval query

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -8,3 +8,24 @@ ORDER BY total_mem DESC;
 
 --query 2
 
+
+CREATE FUNCTION round5(ts timestamp) RETURNS timestamp AS
+    $$
+BEGIN
+RETURN date_trunc('hour', ts) + date_part('minute', ts):: int / 5 * interval '5 min';
+END;
+$$
+LANGUAGE PLPGSQL;
+
+SELECT
+    host_id, hostname, round5(hu.timestamp) as tt, (AVG(total_mem-memory_free)/MAX(total_mem )*100 ) as avg
+
+FROM
+    host_usage as hu
+    JOIN
+    host_info ON hu.host_id=host_info.id
+GROUP BY tt,host_id,hostname
+ORDER BY tt ASC;
+
+
+--query 3
