a49ce93afdfd583a1d8183c2afa56a81e487ae49
Carla <cast4730@mylaurier.ca>
2022-08-08 19:38:59 +0000

    java grepimp

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -2,7 +2,17 @@ package ca.jrvs.apps.grep;
 
 import com.sun.org.slf4j.internal.Logger;
 import com.sun.org.slf4j.internal.LoggerFactory;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.*;
 
 
 public class JavaGrepImp implements JavaGrep {
@@ -16,16 +26,119 @@ public class JavaGrepImp implements JavaGrep {
     if (args.length!=3){
       throw new IllegalArgumentException("USAGE: JavaGrep regex rootPath outFile");
     }
+    // BasicConfigurator.configure();
     JavaGrepImp javaGrepImp= new JavaGrepImp();
     javaGrepImp.setRegex(args[0]);
     javaGrepImp.setRootPath(args[1]);
     javaGrepImp.setOutFile(args[2]);
 
-    BasicConfigurator
+    try{
+      javaGrepImp.process();
+    } catch (Exception ex) {
+      javaGrepImp.logger.error("Error: Unable to process", ex);
 
-    javaGrepImp.logger.error("Error: Unable to process", ex);
+
+    }
+
+
+
 
   }
 
 
+  @Override
+  public void process() throws IOException {
+    List<String> matchedLines= new ArrayList<>();
+
+
+
+  }
+
+  @Override
+  public List<File> listFiles(String rootDir) {
+
+    return null;
+  }
+
+  @Override
+  public List<String> readLines(File inputFile) {
+    List<String> li=new ArrayList<>();
+
+    try {
+      BufferedReader reader = new BufferedReader (new FileReader(inputFile));
+      String line = reader.readLine();
+      while (line!=null){
+        li.add(line);
+        line=reader.readLine();
+      }
+
+    } catch(IOException e){
+      e.printStackTrace();
+    }
+
+
+
+
+    return li;
+  }
+
+  @Override
+  public boolean containsPattern(String line) {
+
+    Pattern p= Pattern.compile(this.regex);
+    Matcher m= p.matcher (line);
+    boolean b = m.matches();
+    return b;
+  }
+
+  @Override
+  public void writeToFile(List<String> lines) throws IOException {
+    try{
+      BufferedWriter bufferedWriter=new BufferedWriter(new FileWriter(this.outFile));
+
+      for (int i=0;i<lines.size();i++)
+        bufferedWriter.write(lines.get(i));
+      bufferedWriter.close();
+
+
+    } catch (IOException e){
+
+    }
+
+
+  }
+
+  @Override
+  public String getRootPath() {
+    return this.rootPath;
+  }
+
+  @Override
+  public void setRootPath(String rootPath) {
+    this.rootPath=rootPath;
+
+  }
+
+  @Override
+  public String getRegex() {
+
+    return this.regex;
+  }
+
+  @Override
+  public void setRegex(String regex) {
+    this.regex= regex;
+
+  }
+
+  @Override
+  public String getOutFile() {
+    return this.getOutFile();
+  }
+
+  @Override
+  public void setOutFile(String outFile) {
+    this.outFile=outFile;
+
+  }
 }
