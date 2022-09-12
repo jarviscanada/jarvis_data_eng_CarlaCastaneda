febdebdf5b3e9e180545b951e7f36582abce2ebf
Carla <cast4730@mylaurier.ca>
2022-09-12 21:44:15 +0000

    CompareMaps

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/CompareMaps.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/CompareMaps.java
@@ -1,6 +1,9 @@
 package ca.jrvs.practice.codingChallenge;
 
+import java.lang.reflect.MalformedParameterizedTypeException;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
 
 public class CompareMaps {
 
@@ -13,6 +16,23 @@ public class CompareMaps {
     Boolean b= m1.equals(m2);
     return b;
 
+  }
+  /**
+   * Big-O:
+   * Justification:
+   */
+  public <K,V> boolean equals(Map<K,V> m1,Map<K,V>m2){
+    if (m1==m2){
+      return true;
+    }
+    if (m1 instanceof Map.Entry<?,?> && m2 instanceof Map.Entry<?,?>){
+      if (Objects.equals(((Entry<?, ?>) m1).getKey(),((Entry<?, ?>) m2).getKey()) &&
+          Objects.equals(((Entry<?, ?>) m1).getValue(),((Entry<?, ?>) m2).getValue()))
+        return true;
+
+    }
+
+    return false;
   }
 
 }
--- practice/src/test/java/ca/jrvs/practice/codingChallenge/CompareMapsTest.java
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/CompareMapsTest.java
@@ -4,15 +4,18 @@ import static org.junit.Assert.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CompareMapsTest {
-
-  @Test
-  public void compareMaps() {
-    CompareMaps c= new CompareMaps();
-    Map<String,String> m1= new HashMap<>();
-    Map<String,String> m2= new HashMap<>();
+  static CompareMaps c;
+  static Map<String,String> m1;
+  static Map<String,String> m2;
+  @BeforeClass
+  public static void setup(){
+    c = new CompareMaps();
+    m1 = new HashMap<>();
+    m2 = new HashMap<>();
 
     m1.put("hi","bye");
     m2.put("hi","bye");
@@ -20,11 +23,23 @@ public class CompareMapsTest {
     m1.put("secret", "my name is johnny");
     m1.put("secret","my name is carla");
     m1.put("dairy","my name is carla");
+    m2.put("dairy","my name is carla");
 
 
-    assertEquals(false,c.compareMaps(m1,m2));
+  }
+
+  @Test
+  public void compareMaps() {
 
 
+    assertEquals(true,c.compareMaps(m1,m2));
+
+
+  }
+  public void equals(){
+
+
+    assertEquals(true,c.equals(m1,m2));
 
 
 
