660c71700f6169c49036d8db05e4bb0664cf7281
Carla <cast4730@mylaurier.ca>
2022-09-13 03:37:41 +0000

    fibonacci dynamic approach

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/FibonacciNumber.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/FibonacciNumber.java
@@ -2,6 +2,12 @@ package ca.jrvs.practice.codingChallenge;
 
 public class FibonacciNumber {
 
+  /**
+   * Big O(n): O(2^n)
+   * Justification: time it takes to find fibonacci number
+   * grows exponentially
+   */
+
   public int fibRec (int i) {
     if (i==1){
       return 1;
@@ -14,5 +20,23 @@ public class FibonacciNumber {
     }
 
   }
+
+  /**
+   * Big-O: O(n)
+   * Justification: Dynamic programming approach
+   */
+  public int fibDynamic(int n){
+    int size=n+1;
+    int [] arrayInt= new int[size];
+    arrayInt[0]=0;
+    arrayInt[1]=1;
+    for(int i=2;i<size;i++){
+      arrayInt[i]=arrayInt[i-1]+arrayInt[i-2];
+
+    }
+    return arrayInt[size-1];
+
+  }
+
 
 }
