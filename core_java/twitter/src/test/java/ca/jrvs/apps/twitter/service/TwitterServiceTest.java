da738e1e687006dbf29f57fa8de51de6f1731058
Carla <cast4730@mylaurier.ca>
2022-09-20 14:42:28 +0000

    unit tests and integration tests

---
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/service/TwitterServiceTest.java
@@ -0,0 +1,20 @@
+package ca.jrvs.apps.twitter.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TwitterServiceTest {
+
+  @Test
+  public void postTweet() {
+  }
+
+  @Test
+  public void showTweet() {
+  }
+
+  @Test
+  public void deleteTweets() {
+  }
+}
