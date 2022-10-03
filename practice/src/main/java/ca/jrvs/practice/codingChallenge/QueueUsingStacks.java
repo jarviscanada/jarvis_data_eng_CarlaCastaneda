258defa9c78af4277164244f20f7891f6d5abe3b
Carla <cast4730@mylaurier.ca>
2022-10-03 15:47:30 +0000

    set up queue using stacks

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/QueueUsingStacks.java
@@ -0,0 +1,32 @@
+package ca.jrvs.practice.codingChallenge;
+
+import java.util.Queue;
+import java.util.Stack;
+
+public class QueueUsingStacks {
+
+
+  public QueueUsingStacks() {
+
+  }
+
+  public void push(int x) {
+
+  }
+
+  public int pop() {
+
+    return 0;
+  }
+
+  public int peek() {
+
+    return 0;
+  }
+
+  public boolean empty() {
+
+    return false;
+  }
+
+}
