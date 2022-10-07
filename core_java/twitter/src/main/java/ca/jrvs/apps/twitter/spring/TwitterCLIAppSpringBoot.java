6fa8fb2512eb79499aa0b58c547949eeaa4746d6
centos <cast4730@mylaurier.ca>
2022-10-07 21:22:19 +0000

    finish springboot

---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/spring/TwitterCLIAppSpringBoot.java
@@ -0,0 +1,29 @@
+package ca.jrvs.apps.twitter.spring;
+
+import ca.jrvs.apps.twitter.controller.TwitterCLIApp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+
+public class TwitterCLIAppSpringBoot implements CommandLineRunner {
+    private TwitterCLIApp app;
+
+    @Autowired
+    public TwitterCLIAppSpringBoot(TwitterCLIApp app){
+        this.app = app;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication app= new SpringApplication(TwitterCLIAppSpringBoot.class);
+
+        app.setWebApplicationType(WebApplicationType.NONE);
+        app.run(args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        app.run(args);
+
+    }
+}
